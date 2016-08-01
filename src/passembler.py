b93051fca6ed449542eff6e9deed341a4202cc6d
zenlc2000 <zenlc2000@gmail.com>
2016-08-01 14:09:27 -0600

    Add files via upload

--- src/passembler.py
+++ src/passembler.py
@@ -9,14 +9,14 @@ def loadData(src):
         print("data from s3")
     elif src.startswith("file://"):
         print("data from file")
-    else
+    else:
         print("unknown data source")
         
 
 
 dsk = {'load-fastq': (loadData, srcfile),
-       'clean-1': (clean, 'load-1'),
-       'clean-2': (clean, 'load-2'),
-       'clean-3': (clean, 'load-3'),
-       'analyze': (analyze, ['clean-%d' % i for i in [1, 2, 3]]),
-       'store': (store, 'analyze')}
+       'clean-1': ('clean', 'load-1'),
+       'clean-2': ('clean', 'load-2'),
+       'clean-3': ('clean', 'load-3'),
+       'analyze': ('analyze', ['clean-%d' % i for i in [1, 2, 3]]),
+       'store'  : ('store', 'analyze')}
